d383363e4a81d55163c971f9f6d49415f32e8be9
Reid Herrera <reidherrerabusiness@gmail.com>
2025-09-07 10:21:11 -0500

    understanding LinQ and extension methods :)

--- Program.cs
+++ Program.cs
@@ -1,40 +1,7 @@
-﻿
+﻿var words = new List<string> { "a", "bb", "ccc", "dddd" };
+var wordsLongerThan2Letters = words.Where(word => word.Length > 2);
+
+var numbers = new int[] { 1, 2, 3, 4, 5, 6 };
+var oddNumbers = numbers.Where(number => number % 2 == 1);
 
 Console.ReadKey();
-
-void Main(string[] args)
-{
-  var wordsNoUpperCase = new string[] {
-    "quick","brown", "fox"
-  };
-  System.Console.WriteLine(IsAnyWordUpperCase(wordsNoUpperCase));
-
-  var wordsWithUppercase = new string[] {
-    "quick", "brown", "FOX"
-  };
-  System.Console.WriteLine(IsAnyWordUpperCase(wordsWithUppercase));
-  Console.ReadKey();
-}
-
-public static bool IsAnyWordUpperCase_Linq(IEnumberable<string> words) {
-  return words.Any(word => word.All(letter => char.IsUpper(letter)));
-}
-
-bool IsAnyWordUpperCase(IEnumerable<string> words) {
-  foreach (var word in words)
-  {
-    bool areAllUppercase = true;
-    foreach (var letter in word)
-    {
-      if (char.IsLower(letter))
-      {
-        areAllUppercase = false;
-      }
-    }
-    if (areAllUppercase)
-    {
-      return true;
-    }
-  }
-  return false;
-}
