4489817a53e5df233d272a8307cb72f6ff61e8f0
Reid Herrera <reidherrerabusiness@gmail.com>
2025-09-07 13:56:47 -0500

    learning the any linq method

--- Program.cs
+++ Program.cs
@@ -2,23 +2,14 @@
 {
   static void Main(string[] args)
   {
-    var words = new List<string> { "a", "bb", "ccc", "dddd" };
-
-    var shortWords = words.Where(word => word.Length < 3);
-
-    System.Console.WriteLine("First Iteration:");
-    foreach (var word in shortWords)
-    {
-      System.Console.WriteLine(word);
-    }
-    words.Add("e");
-
-    System.Console.WriteLine("Second Iteration:");
-    foreach (var word in shortWords)
-    {
-      System.Console.WriteLine(word);
-    }
-
-    Console.ReadKey();
+    var pets = new[] {
+      new Pet(1, "Hannibal", PetType.Fish, 1.1f),
+      new Pet(1, "Hannibal", PetType.Fish, 1.1f),
+      new Pet(1, "Hannibal", PetType.Fish, 1.1f),
+      new Pet(1, "Hannibal", PetType.Fish, 1.1f),
+      new Pet(1, "Hannibal", PetType.Fish, 1.1f),
+      new Pet(1, "Hannibal", PetType.Fish, 1.1f),
+      new Pet(1, "Hannibal", PetType.Fish, 1.1f)
+    };
   }
 }
--- obj/Debug/net8.0/LINQ.AssemblyInfo.cs
+++ obj/Debug/net8.0/LINQ.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LINQ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a34c6e6e3bf16b96bf5f3da299804ed41548453")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb39798982fd51afe9b768f2baab81afa76b6a51")]
 [assembly: System.Reflection.AssemblyProductAttribute("LINQ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LINQ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
