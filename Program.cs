629a4c06ce0f42d2c2fe3583bd9efdb46a71224d
Reid Herrera <reidherrerabusiness@gmail.com>
2025-09-08 08:35:19 -0500

    completed assignment on using select and average methods, though i didnt use select because it wasnt needed.

--- Program.cs
+++ Program.cs
@@ -1,22 +1,8 @@
-﻿var listOfNumbers = new List<List<int>>
-{
-    new List<int> {15,68,20,12,19, 8, 55},
-    new List<int> {12,1,3,4,-19, 8, 7, 6},
-    new List<int> {5,-6,-2,-12,-10, 7}
-};
-
-// var listInformation = listOfNumbers.Select((list, index) => $"List {index + 1}. has {list.Count} values and averages {list.Average()}");
-
-// System.Console.WriteLine(string.Join(Environment.NewLine, listInformation));
-
-var result = listOfNumbers.Select(list => new
-{
-    Count = list.Count(),
-    Average = list.Average()
-}).OrderByDescending(countAndAverage => countAndAverage.Average).Select((list, index) => $"List {index + 1}. has {list.Count} values and averages {list.Average }");
-
-System.Console.WriteLine(string.Join(Environment.NewLine, result));
-
-// var taco = new Containers();
+﻿
 
 Console.ReadKey();
+
+public class Exercise
+    {
+    public static double CalculateAverageDurationInMilliseconds(IEnumerable<TimeSpan> timeSpans) => timeSpans.Average(timespan => timespan.TotalMilliseconds);
+    }
